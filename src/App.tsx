16b29949871e9a0ff36d95cc94196a0bc8611499
py-techninja <84050392+py-techninja@users.noreply.github.com>
2025-11-21 12:53:32 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ScrapeReels from "./component/ScrapeReels";
-import AudioView from "./component/AudioView";
+import ScrapeReels from "../component/ScrapeReels";
+import AudioView from "../component/AudioView";
 
 function App() {
   return (
