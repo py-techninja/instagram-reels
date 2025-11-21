3aad758a83dd56ecc9b46075568ad359d81ad88e
py-techninja <84050392+py-techninja@users.noreply.github.com>
2025-11-21 13:04:40 +0100

    Updated ScrapeReels.tsx

--- src/components/ScrapeReels.tsx
+++ src/components/ScrapeReels.tsx
@@ -3,7 +3,7 @@ import { Play, Pause, Square, Loader2 } from 'lucide-react';
 import { fetchReels } from '../services/instagram';
 import ReelsTable from './ReelsTable';
 import MetadataDisplay from './MetadataDisplay';
-import { Reel, Metadata, FetchReelsResponse } from './types';
+import { Reel, Metadata, FetchReelsResponse } from '../types';
 
 function App() {
   const [input, setInput] = useState('');
