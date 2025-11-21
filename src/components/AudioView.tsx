7e71ebfb88ece789a92300b01f11637ff28c040d
py-techninja <84050392+py-techninja@users.noreply.github.com>
2025-11-21 13:00:27 +0100

    Updated AudioView.tsx

--- src/components/AudioView.tsx
+++ src/components/AudioView.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { getAudioData } from "../supabase/functions/instagram-scraper/index";
+import { getAudioData } from "../../supabase/functions/instagram-scraper/index";
 import MetadataDisplay from "../components/MetadataDisplay";
 import ReelsTable from "../components/ReelsTable";
 
